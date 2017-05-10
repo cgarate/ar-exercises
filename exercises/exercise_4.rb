f48c6cccbcb942c01c0bd36722647d6fd154903f
Carlos Garate <carlos.m.garate@gmail.com>
2017-05-10 22:06:33 +0000

    Looked up stores and looped results. Exercise 4 done.

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,18 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+s = createStore("Surrey", 224000, 0, 1)
+s = createStore("Whistler", 1900000, 1, 0)
+s = createStore("Yaletown", 430000, 1, 1)
+
+@mens_stores = Store.where(mens_apparel:1, womens_apparel:0)
+
+@mens_stores.each do |s|
+  puts "#{s.name} - #{s.annual_revenue}"
+end
+@womens_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000 )
+
+@womens_stores.each do |s|
+  puts "#{s.name} - #{s.annual_revenue}"
+end
