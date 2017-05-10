8e9d70beb183890ebd76bd4a0420b9ce3f13d39e
Carlos Garate <carlos.m.garate@gmail.com>
2017-05-10 21:08:26 +0000

    Exercise 1 finished.

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,21 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+class Store < ActiveRecord::Base
+end
+
+def createStore(name, annual_revenue, mens_apparel, womens_apparel)
+    s = Store.new
+    s.name = name
+    s.annual_revenue = annual_revenue
+    s.mens_apparel = mens_apparel
+    s.womens_apparel = womens_apparel
+    s.save
+end
+
+s1 = createStore("Burnaby", 300000, 1, 1)
+s2 = createStore("Richmond", 1260000, 0, 1)
+s3 = createStore("Gastown", 190000, 1, 0)
+
+puts Store.count
